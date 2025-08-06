5f7a3cc521525b2db20a35371805673b55ee888b
Shatlyk <90685859+Shatlyk1011@users.noreply.github.com>
2025-08-06 17:40:57 +0500

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -11,17 +11,6 @@ export default createStore({
         github: "https://github.com/Shatlyk1011/live-room",
         img: "https://images.unsplash.com/photo-1577563908411-5077b6dc7624?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
       },
-
-      {
-        title: "Natours",
-        descr:
-          "Landing page со сложными компонентами. На этом сайте модальное окно(при нажатии на карточки) реализовано на чистом CSS. Однозначно, стоит вашего вниманиz.",
-        site: "https://outdoors-shatlyk.netlify.app",
-        github: "https://github.com/Shatlyk1011/natours",
-        img: "https://images.unsplash.com/photo-1532339142463-fd0a8979791a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-        order: "order",
-        orderGrid: "orderGrid",
-      },
       {
         title: "Google Works",
         descr:
